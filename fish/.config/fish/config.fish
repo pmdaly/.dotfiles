3fd1b540d8e4bcb0bf10fb3a8c46e8bf1322e0f8
Patrick Daly <pmdaly12@gmail.com>
2023-01-17 06:44:34 -0800

    updating fish/kitty

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,13 +3,16 @@ set fish_key_bindings fish_user_key_bindings
 
 # No greeting
 set -g fish_greeting
-set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
+fish_add_path ~/.cargo/bin
+fish_add_path "/usr/local/opt/ruby/bin"
 set NPM_PACKAGES "$HOME/.npm-packages"
 set PATH $PATH $NPM_PACKAGES/bin
 set MANPATH $NPM_PACKAGES/share/man $MANPATH
+fish_add_path /opt/homebrew/bin
+fish_add_path /Users/pmd/Library/Python/3.8/bin
 
 # Source Autojump
-[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+[ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
 
 # should aliases be in a function?
 alias ll='ls -alF -G'
@@ -20,3 +23,9 @@ alias fp="nvim (ff)"
 alias c="cd"
 alias vim="nvim"
 alias icat="kitty +kitten icat"
+# alias cat="ccat"
+
+# Created by `pipx` on 2022-01-15 05:11:31
+set PATH $PATH /Users/pmd/.local/bin
+
+export PATH="$PATH:/Users/pmd/.foundry/bin"
