076db6479087a80742225bb97f877c9fcebbb12f
Patrick Daly <pmdaly12@gmail.com>
2025-06-19 11:53:37 -0700

    slight refactor

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -46,6 +46,34 @@ return {
             -- Note: autocompletion settings will not take effect
 
             require('lsp-zero.settings').preset({})
+
+            -- Configure LSP servers
+            local lsp = require('lsp-zero')
+
+            lsp.on_attach(function(client, bufnr)
+                -- see :help lsp-zero-keybindings
+                -- to learn the available actions
+                lsp.default_keymaps({buffer = bufnr})
+            end)
+
+            -- Configure specific LSP servers
+            lsp.configure('lua_ls', {
+                settings = lsp.nvim_lua_ls()
+            })
+
+            lsp.configure('pyright', {
+                settings = {
+                    python = {
+                        analysis = {
+                            autoSearchPaths = true,
+                            diagnosticMode = "openFilesOnly",
+                            useLibraryCodeForTypes = true
+                        }
+                    }
+                }
+            })
+
+            lsp.setup()
         end
     },
     -- Autocompletion
@@ -110,35 +138,5 @@ return {
             {'williamboman/mason-lspconfig.nvim'},
             {'williamboman/mason.nvim'},
         },
-        config = function()
-            -- This is where all the LSP shenanigans will live
-
-            local lsp = require('lsp-zero')
-
-            lsp.on_attach(function(client, bufnr)
-                -- see :help lsp-zero-keybindings
-                -- to learn the available actions
-                lsp.default_keymaps({buffer = bufnr})
-            end)
-
-            -- (Optional) Configure lua language server for neovim
-            require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-            require('lspconfig').pyright.setup{}
-
-            lsp.setup()
-        end
-    },
-    {
-        "ray-x/lsp_signature.nvim",
-        event = "LspAttach",
-        config = function()
-            require("lsp_signature").setup({
-                bind = true,
-                handler_opts = {
-                    border = "rounded"
-                },
-                hint_enable = true,
-            })
-        end,
     },
 }
